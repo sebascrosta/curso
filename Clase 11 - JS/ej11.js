56d6475bb078dead6355ae499647befcf9c8c201
Digital House <digitalhouse@DH-A3-14.digitalhouse.local>
2016-08-31 21:56:21 -0300

    Clase 15

---
+++ Clase 11 - JS/ej11.js
@@ -0,0 +1,3 @@
+//Utilizando la función map y  el operador %​ realizar un programa que pase un arrayde números positivos a texto en donde cada número (del 1 al 27) representa una letra (de la A a la Z incluyendo la Ñ).
+
+var numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
